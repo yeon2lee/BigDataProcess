2881079f2641af185d28c504b29d700b6eb434d7
yeon2lee <md04254@naver.com>
2022-10-29 02:23:33 +0900

    [UPDATE] IMDB process code

--- HW3/IMDBStudent20200994.py
+++ HW3/IMDBStudent20200994.py
@@ -1,2 +1,21 @@
 #/user/bin/python3
 
+import sys
+
+inputfile = sys.argv[1] 
+outputfile = sys.argv[2]
+
+count_genre = dict()
+with open(inputfile, "rt") as fp:
+	for line in fp:
+		movie = line.split("::")
+		genre_list = movie[2].strip().split("|")
+		for genre in genre_list:
+			if genre not in count_genre:
+				count_genre[genre] = 1
+			else:
+				count_genre[genre] += 1
+
+with open(outputfile, "wt") as fp:
+	for genre in count_genre:
+		fp.write("{} {}\n".format(genre, count_genre[genre]))
