28c52dae9643606d8dfcc932c9045b8eb6ed5455
yeon2lee <md04254@naver.com>
2022-10-30 00:45:59 +0900

    [UPDATE] find each region & day

--- HW3/UBERStudent20200994.py
+++ HW3/UBERStudent20200994.py
@@ -6,16 +6,24 @@ import datetime
 inputfile = sys.argv[1]
 outputfile = sys.argv[2]
 
-uber_list = []
+uber_list = dict()
 days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
 with open(inputfile, "rt") as fp:
 	for line in fp:
 		uber = line.strip().split(",")
 		day = uber[1].split("/")
 		day = days[datetime.date(int(day[2]), int(day[0]), int(day[1])).weekday()]
-		uber_list.append([uber[0], day, uber[2], uber[3]])
+		if uber[0] not in uber_list:
+			uber_list[uber[0]] = {}
+
+		if day not in uber[0]:
+			uber_list[uber[0]][day] = [uber[2], uber[3]]
+		else:		
+			uber_list[uber[0]][day][0] += uber[2]
+			uber_list[uber[0]][dat][1] += uber[3] 
 	
 with open(outputfile, "wt") as fp:
 	for uber in uber_list:
-		fp.write("{},{}\t{},{}\n".format(uber[0], uber[1], uber[2], uber[3]))
+		for day in uber_list[uber]:
+			fp.write("{},{}\t{},{}\n".format(uber, day, uber_list[uber][day][0], uber_list[uber][day][1]))
 		
