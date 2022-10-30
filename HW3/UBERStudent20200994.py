8ed61b4ca386921f6e2ec4ea75dbf3f10793a03d
yeon2lee <md04254@naver.com>
2022-10-31 02:37:09 +0900

    [UPDATE] change library datetime to calendar

--- HW3/UBERStudent20200994.py
+++ HW3/UBERStudent20200994.py
@@ -1,25 +1,26 @@
 #/user/bin/python3
 
 import sys
-import datetime
+import calendar
 
 inputfile = sys.argv[1]
 outputfile = sys.argv[2]
 
-uber_list = dict()
+uber_dict = dict()
 days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
 with open(inputfile, "rt") as fp:
 	for line in fp:
 		uber = line.strip().split(",")
 		day = uber[1].split("/")
-		day = days[datetime.date(int(day[2]), int(day[0]), int(day[1])).weekday()]
+		day = days[calendar.weekday(int(day[2]), int(day[0]), int(day[1]))
+
 		info = (uber[0], day)
-		if info not in uber_list:
-			uber_list[info] = [int(uber[2]), int(uber[3])]
+		if info not in uber_dict:
+			uber_dict[info] = [int(uber[2]), int(uber[3])]
 		else:
-			uber_list[info][0] += int(uber[2])
-			uber_list[info][1] += int(uber[3])
+			uber_dict[info][0] += int(uber[2])
+			uber_dict[info][1] += int(uber[3])
 
 with open(outputfile, "wt") as fp:
-	for uber in uber_list:
-		fp.write("{},{} {},{}\n".format(uber[0], uber[1], uber_list[uber][0], uber_list[uber][1]))		
+	for uber in uber_dict:
+		fp.write("{},{} {},{}\n".format(uber[0], uber[1], uber_dict[uber][0], uber_dict[uber][1]))		
