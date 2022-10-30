4cfddfd3bf0a330489e29798aff8876457db8dba
yeon2lee <md04254@naver.com>
2022-10-31 03:49:28 +0900

    [UPDATE] dictionary method

--- HW3/UBERStudent20200994.py
+++ HW3/UBERStudent20200994.py
@@ -1,4 +1,4 @@
-#/user/bin/python3
+#!/usr/bin/python3
 
 import sys
 import calendar
@@ -7,20 +7,20 @@ inputfile = sys.argv[1]
 outputfile = sys.argv[2]
 
 uber_dict = dict()
-days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+days = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
 with open(inputfile, "rt") as fp:
 	for line in fp:
 		uber = line.strip().split(",")
-		day = uber[1].split("/")
-		day = days[calendar.weekday(int(day[2]), int(day[0]), int(day[1]))]
+		day_list = uber[1].split("/")
+		day = days[calendar.weekday(int(day_list[2]), int(day_list[0]), int(day_list[1]))]
 
-		info = (uber[0], day)
-		if info not in uber_dict:
-			uber_dict[info] = [int(uber[2]), int(uber[3])]
+		key = (uber[0], day)
+		if key not in uber_dict:
+			uber_dict[key] = [int(uber[2]), int(uber[3])]
 		else:
-			uber_dict[info][0] += int(uber[2])
-			uber_dict[info][1] += int(uber[3])
+			uber_dict[key][0] += int(uber[2])
+			uber_dict[key][1] += int(uber[3])
 
 with open(outputfile, "wt") as fp:
-	for uber in uber_dict:
-		fp.write("{},{} {},{}\n".format(uber[0], uber[1], uber_dict[uber][0], uber_dict[uber][1]))		
+	for key, value in uber_dict.items():
+		fp.write("{},{} {},{}\n".format(key[0], key[1], value[0], value[1]))		
