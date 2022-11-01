b65d216c78b6595e307684b1b157d9d53a8365ca
yeon2lee <md04254@naver.com>
2022-11-02 06:20:27 +0900

    [DELETE] example files

--- HW3/UBERStudent20200994.py
+++ HW3/UBERStudent20200994.py
@@ -12,8 +12,9 @@ with open(inputfile, "rt") as fp:
 	for line in fp:
 		uber = line.strip().split(",")
 		day_list = uber[1].split("/")
-		day = days[calendar.weekday(int(day_list[2]), int(day_list[0]), int(day_list[1]))]
-
+		tmp = calendar.weekday(int(day_list[2]), int(day_list[0]), int(day_list[1]))
+		day = days[tmp]
+		
 		key = (uber[0], day)
 		if key not in uber_dict:
 			uber_dict[key] = [int(uber[2]), int(uber[3])]
@@ -21,6 +22,9 @@ with open(inputfile, "rt") as fp:
 			uber_dict[key][0] += int(uber[2])
 			uber_dict[key][1] += int(uber[3])
 
+result = ""
+for key, value in uber_dict.items():
+	result += key[0] + "," + key[1] + " " + str(value[0]) + "," + str(value[1]) + "\n"
+
 with open(outputfile, "wt") as fp:
-	for key, value in uber_dict.items():
-		fp.write("{},{} {},{}\n".format(key[0], key[1], value[0], value[1]))		
+	fp.write(result)
