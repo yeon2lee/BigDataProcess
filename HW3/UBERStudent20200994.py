11b0b6c0af0b66adde3976c2ea3ebfa9efdc94f1
yeon2lee <md04254@naver.com>
2022-11-02 06:38:06 +0900

    [UPDATE] delete function

--- HW3/UBERStudent20200994.py
+++ HW3/UBERStudent20200994.py
@@ -10,7 +10,7 @@ uber_dict = dict()
 days = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
 with open(inputfile, "rt") as fp:
 	for line in fp:
-		uber = line.strip().split(",")
+		uber = line.split(",")
 		day_list = uber[1].split("/")
 		tmp = calendar.weekday(int(day_list[2]), int(day_list[0]), int(day_list[1]))
 		day = days[tmp]
@@ -22,8 +22,10 @@ with open(inputfile, "rt") as fp:
 			uber_dict[key][0] += int(uber[2])
 			uber_dict[key][1] += int(uber[3])
 
+keys = uber_dict.keys()
 result = ""
-for key, value in uber_dict.items():
+for key in keys:
+	value = uber_dict[key]
 	result += key[0] + "," + key[1] + " " + str(value[0]) + "," + str(value[1]) + "\n"
 
 with open(outputfile, "wt") as fp:
